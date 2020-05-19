62486e300fa3287e439fb1d58e9169800c3bb282
Alistair Martin <alistairmartin01@gmail.com>
2020-05-19 18:22:23 +0800

    Refactor doc2vec a bit.

--- doc2vec_model.py
+++ doc2vec_model.py
@@ -8,7 +8,7 @@ def read_templates(fname, tokens_only=False):
         result = []
         ID_cache = {}
         for i, row in enumerate(reader):
-            row_text = re.sub("\*\**\*\*", "", row['Text'])
+            row_text = re.sub("\*\*.*\*\*('s)?", "", row['Text'])
             tokens = gensim.utils.simple_preprocess(row_text)
             # tag = [float(row['Identifier']) + float(row['Version No']) / 100]
             # tag = [int(row['Identifier']), int(row['Version No']) ]
@@ -18,22 +18,15 @@ def read_templates(fname, tokens_only=False):
             ID_cache[i] = row['Identifier'] + '.' + row['Version No']
         return result, ID_cache
 
-
-if __name__ == '__main__':
-    start_time = time.time()
-    templates, ID_version = list(read_templates(os.getcwd() + "/tenement_templates_dupes_removed.csv"))
-    print("Imported {0} conditions templates in {1:.2f} seconds\n".format(len(templates), time.time() - start_time))
-
-    #  print(templates[0:10]) # sanity check for correct import
-
+def train_model(templates):
     print("Training the model")
     start_time = time.time()
-    model = gensim.models.doc2vec.Doc2Vec(documents=templates, vector_size=50, min_count=2, epochs=200, dm=1)
-    print("Model trained in {0:.2f} seconds\n".format(time.time() - start_time))
-
+    model = gensim.models.doc2vec.Doc2Vec(documents=templates, vector_size=50, min_count=2, epochs=1000, dm=1)
     assert len(model.docvecs) == len(templates), "Uh oh! The number of trained vectors doesn't equal the number of conditions templates."
+    print("Model trained in {0:.2f} seconds\n".format(time.time() - start_time))
+    return model
 
-    # Assess the model with the training data (sanity check)
+def vector_match(model, templates):
     print("Matching the documents")
     start_time = time.time()
 
@@ -57,12 +50,24 @@ if __name__ == '__main__':
     print(counter)
     # Correct Identifier matches (with different Version numbers)
     print("{} correct out of {} templates... {}%".format(count_correct, len(templates), count_correct / len(templates)))
-
     print("{0} documents compared in {1:.2f} seconds\n".format(len(ranks), time.time() - start_time))
+    return first_ranks, second_ranks
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    templates, ID_version = list(read_templates(os.getcwd() + "/tenement_templates_dupes_removed.csv"))
+    print("Imported {0} conditions templates in {1:.2f} seconds\n".format(len(templates), time.time() - start_time))
+
+    #  print(templates[0:10]) # sanity check for correct import
+    model = train_model(templates)
+
+    # Assess the model with the training data (sanity check)
+    first_ranks, second_ranks = vector_match(model, templates)
 
     # Similar matches may just be a different Version of the same Identifier
     # Check second best matches that rank highly, but don't have the same Identifier
-    for tID in range(len(templates)):
+    for tID in range(len(templates[:10])):
         second = second_ranks[tID]
         if templates[tID].tags is not second[0] and second[1] > 0.9 and int(float(ID_version[tID])) is not int(float(ID_version[second[0]])):
             print('Train Document ({}), ID-{}: «{}»'.format(tID, ID_version[tID], ' '.join(templates[tID].words)))
