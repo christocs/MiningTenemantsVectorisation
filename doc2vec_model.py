2f48a6b7c8dea3648d3beb2b0ff2fe0a7acb3911
Alistair Martin <alistairmartin01@gmail.com>
2020-05-25 00:48:55 +0800

    Refactor to allow reporting required, and set up to import conditions.

--- doc2vec_model.py
+++ doc2vec_model.py
@@ -1,75 +1,115 @@
 import os, time, csv, re, random
 import gensim
+from gensim.models.doc2vec import TaggedDocument
 import collections
 
-def read_templates(fname, tokens_only=False):
+NEAR_MATCH_THRESHOLD = 0.03 # how close a template match must be to the highest match to be considered significant
+
+class Template(TaggedDocument):
+    def __new__(self, tokens, tags, id, version):
+        obj = TaggedDocument.__new__(self, tokens, tags)
+        obj.id = id
+        obj.version = version
+        return obj
+
+def read_conditions(fname, condition_column_name):
+    start_time = time.time()
+    result = []
     with open(fname, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
-        result = []
-        ID_cache = {}
         for i, row in enumerate(reader):
-            row_text = re.sub("\*\*.*\*\*('s)?", "", row['Text'])
+            tokens = gensim.utils.simple_preprocess(row[condition_column_name])
+            result.append(tokens)
+    print("Imported {0} conditions in {1:.2f} seconds\n".format(len(result), time.time() - start_time))
+    return result
+
+def read_templates(fname, template_column_name = "Text"):
+    start_time = time.time()
+    result = []
+    ID_cache = {}
+    with open(fname, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for i, row in enumerate(reader):
+            row_text = re.sub("\*\*.*\*\*('s)?", "", row[template_column_name])
             tokens = gensim.utils.simple_preprocess(row_text)
-            # tag = [float(row['Identifier']) + float(row['Version No']) / 100]
-            # tag = [int(row['Identifier']), int(row['Version No']) ]
-            # Apparently the tag must be a list with a single, unique integer
-            tag = [i]
-            result.append(gensim.models.doc2vec.TaggedDocument(tokens, tag))
+            result.append(Template(tokens, [i], int(row['Identifier']), int(row['Version No'])))
             ID_cache[i] = row['Identifier'] + '.' + row['Version No']
-        return result, ID_cache
+    print("Imported {0} conditions templates in {1:.2f} seconds\n".format(len(result), time.time() - start_time))
+    return result, ID_cache
 
 def train_model(templates):
     print("Training the model")
     start_time = time.time()
-    model = gensim.models.doc2vec.Doc2Vec(documents=templates, vector_size=50, min_count=2, epochs=1000, dm=1)
+    model = gensim.models.doc2vec.Doc2Vec(documents=templates, vector_size=50, min_count=2, epochs=200, dm=1)
     assert len(model.docvecs) == len(templates), "Uh oh! The number of trained vectors doesn't equal the number of conditions templates."
     print("Model trained in {0:.2f} seconds\n".format(time.time() - start_time))
     return model
 
-def vector_match(model, templates):
+
+def vector_match(model, templates, conditions = []): 
     print("Matching the documents")
     start_time = time.time()
 
-    count_correct = 0
-    ranks = []
-    second_ranks = []
-    first_ranks = []
-    for tID in range(len(templates)):
-        vector = model.infer_vector(templates[tID].words)
-        sims = model.docvecs.most_similar([vector], topn=len(model.docvecs))
-        rank = [docid for docid, sim in sims].index(tID)
-        ranks.append(rank)
-        if int(float(ID_version[sims[0][0]])) == int(float(ID_version[tID])):
-            count_correct += 1 
+    # conditions is expected to be a list of list of words
+    if len(conditions) == 0: # if no conditions are supplied, just match templates against themselves
+        conditions = [" ".join(t.words) for t in templates]
+    single_matches = {}
+    multiple_matches = {}
+    for cID, condition in enumerate(conditions):
+        condition_vector = model.infer_vector(gensim.utils.simple_preprocess(condition))
+        most_similar = model.docvecs.most_similar([condition_vector], topn=5) 
+        highest_score = most_similar[0][1]
 
-        first_ranks.append(sims[0])
-        second_ranks.append(sims[1])
-    
-    # Exact matches
-    counter = collections.Counter(ranks)
-    print(counter)
-    # Correct Identifier matches (with different Version numbers)
-    print("{} correct out of {} templates... {}%".format(count_correct, len(templates), count_correct / len(templates)))
-    print("{0} documents compared in {1:.2f} seconds\n".format(len(ranks), time.time() - start_time))
-    return first_ranks, second_ranks
+        # determine how many other templates rank close to this one
+        # TODO: this should probably only consider DIFFERENT TEMPLATE IDs, not different versions of the same ID
+        num_close_matches = sum(1 for tID, score in most_similar[1:] if abs(highest_score - score) < NEAR_MATCH_THRESHOLD)
+        
+        if num_close_matches == 0:
+            highest = templates[most_similar[0][0]]
+            single_matches[cID] = (highest_score, highest.id, highest.version)
+        else:
+            multiple_matches[cID] = [(sim[1], templates[sim[0]].id, templates[sim[0]].version) for sim in most_similar[0:num_close_matches+1]]
+    print("{0} documents compared in {1:.2f} seconds\n".format(len(conditions), time.time() - start_time))
+    return single_matches, multiple_matches
 
 
 if __name__ == '__main__':
-    start_time = time.time()
     templates, ID_version = list(read_templates(os.getcwd() + "/tenement_templates_dupes_removed.csv"))
-    print("Imported {0} conditions templates in {1:.2f} seconds\n".format(len(templates), time.time() - start_time))
-
-    #  print(templates[0:10]) # sanity check for correct import
     model = train_model(templates)
 
     # Assess the model with the training data (sanity check)
-    first_ranks, second_ranks = vector_match(model, templates)
+    matched, uncertain = vector_match(model, templates)
+    print("{0} documents matches exactly, {1} have multiple matches\n".format(len(matched), len(uncertain)))
+    
+    ranks = []
+    for ID, matches in uncertain.items():
+        ranks.append(len(matches))
+    counter = collections.Counter(ranks)
+    print("The number of matches per uncertain document are:")
+    print(counter)
+
+    # TODO: testing
+    vector = model.infer_vector(templates[23].words)
+    most_similar = model.docvecs.most_similar([vector], topn=len(model.docvecs))
+    #  print(most_similar[:5])
+
+    # Match each condition with the templates
+    # Determine which conditions have 
+        # just one match
+        # > 1 match
+    # write the results to disk
+    # output a count of the results
+    # output a graph
+    # determine how many of the conditions are duplicates
+
+
+
 
     # Similar matches may just be a different Version of the same Identifier
     # Check second best matches that rank highly, but don't have the same Identifier
-    for tID in range(len(templates[:10])):
-        second = second_ranks[tID]
-        if templates[tID].tags is not second[0] and second[1] > 0.9 and int(float(ID_version[tID])) is not int(float(ID_version[second[0]])):
-            print('Train Document ({}), ID-{}: «{}»'.format(tID, ID_version[tID], ' '.join(templates[tID].words)))
-            print('Similar Document {}, ID-{}: «{}»\n'.format(second[0], ID_version[second[0]], ' '.join(templates[second[0]].words)))
+    #  for tID in range(len(templates[:10])):
+        #  second = second_ranks[tID]
+        #  if templates[tID].tags is not second[0] and second[1] > 0.9 and int(float(ID_version[tID])) is not int(float(ID_version[second[0]])):
+            #  print('Train Document ({}), ID-{}: «{}»'.format(tID, ID_version[tID], ' '.join(templates[tID].words)))
+            #  print('Similar Document {}, ID-{}: «{}»\n'.format(second[0], ID_version[second[0]], ' '.join(templates[second[0]].words)))
 
